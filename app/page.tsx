8f3ad6a6e7ef791cc95074db0ebf013a2d788d60
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-05-17 12:29:27 +0530

    update: Main NextJs code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { MainPage } from "@/components/component/main-page";
+
+export default function Home() {
+  return (
+    <main className="">
+      <MainPage />
+    </main>
+  );
+}
