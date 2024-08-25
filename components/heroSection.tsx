e2c302e01ba89283cb455ae6ae3cf9944a13cece
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-08-25 17:32:30 +0530

    update: main Hero

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="w-full py-12 md:py-10 lg:py-10 bg-gradient-to-r from-[#2c5282] to-[#4c6ef5] mt-16">
-      <div className="px-4 md:px-6 text-white">
+    <section className="w-full py-12 md:py-10 lg:py-10 mt-16 relative overflow-hidden">
+      <video 
+        className="absolute inset-0 w-full h-full object-cover z-0" 
+        autoPlay 
+        loop 
+        muted 
+        playsInline
+      >
+        <source src="/promo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="relative px-4 md:px-6 text-white z-10">
         <div className="grid items-center px-10 gap-6 lg:grid-cols-2 lg:gap-12">
           <div className="space-y-4">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-7xl">
@@ -37,7 +47,7 @@ export default function HeroSection() {
           </div>
           <img
             alt="Hero"
-            className="mx-auto aspect-square overflow-hidden rounded-xl object-cover transition-transform transition-filter duration-300 ease-in-out hover:brightness-110 hover:scale-105 "
+            className="mx-auto aspect-square overflow-hidden rounded-xl object-cover shadow-lg shadow-slate-800 transition-transform transition-filter duration-300 ease-in-out hover:brightness-110 hover:scale-105"
             height="550"
             src="/main.jpg"
             width="550"
