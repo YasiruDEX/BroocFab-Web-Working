7718c455a10ce492406e5b728ef31e5abc75964d
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2025-06-15 03:35:51 +0530

    Update layout.tsx

--- app/structure-lab/layout.tsx
+++ app/structure-lab/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'BroocFab',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
